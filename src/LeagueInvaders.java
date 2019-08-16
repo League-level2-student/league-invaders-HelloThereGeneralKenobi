634be8f9872e6ec4549771cbcfc4f307022b208d
League Student <league@iMac-5.attlocal.net>
2019-08-16 16:26:46 -0700

    Started LeagueInvaders
    I did the LeagueInvaders class, the GamePanel class, and GameObject class.

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,30 @@
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	
+	JFrame frame;
+	final int width = 500;
+	final int height = 800;
+	Component GamePanel;
+	
+	public static void main(String[] args) {
+		
+		LeagueInvaders (JFrame frame) {
+			
+		}
+		
+		LeagueInvaders object = new LeagueInvaders();
+		object.setup();
+	}
+		void setup() {
+			frame = new JFrame();
+			frame.add(GamePanel);
+			frame.setVisible(true);
+			frame.getContentPane().setPreferredSize(new Dimension(width, height));
+			frame.pack();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
 }
